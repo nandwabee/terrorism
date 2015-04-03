cf4cf8e4d40fb5f886200d1ba94c5f993ad2ae8c
Bernard Nandwa <nandwabee@gmail.com>
2015-04-03 12:46:52 +0300

    Some three states to start work with.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,25 @@
-var app = angular.module('SecurityTracker',['ui.router']);
+var SecurityTracker = angular.module('SecurityTracker',['ui.router']);
+
+SecurityTracker.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/');
+    
+    $stateProvider
+        
+        .state('home', {
+            url: '/',
+            templateUrl: 'partials/home.html'
+        })
+        
+        .state('about', {
+            url: '/about',
+            templateUrl: 'partials/about.html',
+        })
+        
+        .state('code', {
+            url: '/code',
+            templateUrl: 'partials/code.html'
+        });
+        
+});
 
